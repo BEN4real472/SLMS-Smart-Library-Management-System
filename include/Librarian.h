1dda8a5e7ea0e082bcccc5c781b42d34eadf958f
BEN4real472 <benjaminegoro60@gmail.com>
2026-03-04 01:12:57 +0000

    Add Librarian class with book management methods

--- include/Librarian.h
+++ include/Librarian.h
@@ -1,1 +1,26 @@
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
 
+#include "User.h"
+#include <string>
+
+using namespace std;
+
+class Librarian : public User {
+
+private:
+    int userId;
+
+public:
+
+    Librarian(string id, string n, string e, string p);
+
+    void addBook(string bookId);
+    void updateBook(string bookId);
+    void removeBook(string bookId);
+    void generateReport();
+    void librarianDisplayDashboard();
+
+};
+
+#endif
