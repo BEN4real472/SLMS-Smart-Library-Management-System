26f7114249bfaf7eaf38f3884616975baaf17d98
BEN4real472 <benjaminegoro60@gmail.com>
2026-03-04 12:13:18 +0000

    Implement Book class with status and due date management

---
+++ src/Book.cpp
@@ -0,0 +1,33 @@
+#include "../include/Book.h"
+#include <iostream>
+
+using namespace std;
+
+Book::Book(string id, string t, string a)
+{
+    bookId = id;
+    title = t;
+    author = a;
+    status = "Available";
+    dueDate = "N/A";
+}
+
+string Book::getStatus()
+{
+    return status;
+}
+
+void Book::setStatus(string newStatus)
+{
+    status = newStatus;
+}
+
+void Book::setDueDate(string newDueDate)
+{
+    dueDate = newDueDate;
+}
+
+string Book::getDueDate()
+{
+    return dueDate;
+}
