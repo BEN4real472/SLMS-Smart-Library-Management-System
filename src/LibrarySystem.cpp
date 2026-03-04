2bfde2345f09687c9d68ca85b6001921a459077a
BEN4real472 <benjaminegoro60@gmail.com>
2026-03-04 12:19:48 +0000

    Implement LibrarySystem class with basic functions

---
+++ src/LibrarySystem.cpp
@@ -0,0 +1,24 @@
+#include "../include/LibrarySystem.h"
+#include <iostream>
+
+using namespace std;
+
+LibrarySystem::LibrarySystem()
+{
+    cout << "Library System initialized." << endl;
+}
+
+void LibrarySystem::startSystem()
+{
+    cout << "Starting Smart Library Management System..." << endl;
+}
+
+void LibrarySystem::shutdownSystem()
+{
+    cout << "Shutting down Library System..." << endl;
+}
+
+void LibrarySystem::displaySystemStatus()
+{
+    cout << "System is running normally." << endl;
+}
